56714a6033cefa0b9a2aebac02f77d3d3f58292f
dan <80271815+poonchoi@users.noreply.github.com>
2021-10-01 12:13:38 +0200

    Update OOP-practice.py

--- Computer Science/ms nethercote/Class work/OOP-practice.py
+++ Computer Science/ms nethercote/Class work/OOP-practice.py
@@ -1,19 +1,43 @@
+'''
 
-def task2():
-    class Animal:
-        def __init__(self, s, n):
-            self.state = s
-            self.size = n
+# task 2
+class Animal:
+    def __init__(self, s, n):
+        self.state = s
+        self.size = n
 
-        def feed(self):
-            self.size += 1
-            if self.size == 5:
-                self.state = "FISH"
+    def feed(self):
+        self.size += 1
+        if self.size == 5:
+            self.state = "FISH"
 
-    animal1 = Animal(1, "fish")
-    print(animal1.feed)
-            
-    
+    def getState(self):
+        return self.state
+
+    def getSize(self):
+        return self.size
+
+# task 3
+thisFish = Animal("Fish", 1)
+
+print(f"{thisFish.getSize()} is the size of {thisFish.getState()}")
+
+while thisFish.getState() != "FISH":
+    thisFish.feed()
+print(f"{thisFish.getState()} is now {thisFish.getSize()} in size")
+
+print(f"it is now a big {thisFish.getState()}")
+
+'''
+
+# task 4
+
+class car:
+    def __init__(self, registration, make, mileage, dateOfInspection):
+       self.registration = registration
+       self.make = make
+       self.mileage = 0
+       self.dateOfInspection = dateOfInspection
 
 
 

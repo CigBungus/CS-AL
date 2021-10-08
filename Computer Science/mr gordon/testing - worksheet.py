87d347f034678ec37042b7424aca7441252a6634
dan <80271815+poonchoi@users.noreply.github.com>
2021-10-08 09:34:41 +0200

    Update testing - worksheet.py

--- Computer Science/mr gordon/testing - worksheet.py
+++ Computer Science/mr gordon/testing - worksheet.py
@@ -1,18 +1,33 @@
-temp = []
 average = []
-
+daily_temp = []
+day = 0
 while True:
+    day += 1
+    print(f"day #{day}")
     max_temp = int(input("MAX TEMPERATURE: "))
     if max_temp == 999:
-        break
-        
+        break   
+
     min_temp = int(input("MIN_TEMPERATURE: "))
 
+    average_of_day = (max_temp + min_temp) / 2
     
-    temp.append(list(max_temp))
-    temp.append(list(min_temp))
+    average.append(average_of_day)
+    daily_temp.append(max_temp)
+    daily_temp.append(min_temp)
 
-    print(temp)
+days = len(average)
 
+total_average = sum(average) / 2
+
+for i in range(days-1):
+    if average[i] == total_average:
+        print(f"on day #{i+1} the temperature was the same as the average temperature")
+
+    elif average[i] > total_average:
+        print(f"on day #{i+1} the temperature was higher than the average temperature")
+        
+    elif average[i] < total_average:
+        print(f"on day #{i+1} the temperature was lower than the average temperature")
 
 

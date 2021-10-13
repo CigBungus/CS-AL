2b9fe0a4d5be751e4e439201c351c9ab459008e4
dan <80271815+poonchoi@users.noreply.github.com>
2021-10-13 22:54:20 +0200

    Update subprogram coding drills.py

--- Computer Science/Practice/subprogram coding drills.py
+++ Computer Science/Practice/subprogram coding drills.py
@@ -45,39 +45,39 @@ import random
 
 # 120
 
-def menu():
-    print("1) Addition\n2) Subtraction")
-    menu_choice = int(input("Enter 1 or 2: "))
+# def menu():
+#     print("1) Addition\n2) Subtraction")
+#     menu_choice = int(input("Enter 1 or 2: "))
     
-    if menu_choice == 1:
-        addition()
+#     if menu_choice == 1:
+#         addition()
 
-    elif menu_choice == 2:
-        subtraction()
+#     elif menu_choice == 2:
+#         subtraction()
 
-def addition():
-    num1 = random.randint(5, 20)
-    num2 = random.randint(5, 20)
-    answer = num1 + num2
-    user_answer = int(input(f"what is {num1}+{num2}? "))
-    check(answer, user_answer)
-    return answer, user_answer
+# def addition():
+#     num1 = random.randint(5, 20)
+#     num2 = random.randint(5, 20)
+#     answer = num1 + num2
+#     user_answer = int(input(f"what is {num1}+{num2}? "))
+#     check(answer, user_answer)
+#     return answer, user_answer
 
-def subtraction():
-    num1 = random.randint(25, 50)
-    num2 = random.randint(1, 25)
-    answer = num1 - num2
-    user_answer = int(input(f"what is {num1}-{num2}? "))
-    check(answer, user_answer)
-    return answer, user_answer
+# def subtraction():
+#     num1 = random.randint(25, 50)
+#     num2 = random.randint(1, 25)
+#     answer = num1 - num2
+#     user_answer = int(input(f"what is {num1}-{num2}? "))
+#     check(answer, user_answer)
+#     return answer, user_answer
 
-def check(answer, user_answer):
-    if answer == user_answer:
-        print("correct")
-    else:
-        print(f"incorrect\nthe correct answer is {answer}")
+# def check(answer, user_answer):
+#     if answer == user_answer:
+#         print("correct")
+#     else:
+#         print(f"incorrect\nthe correct answer is {answer}")
 
-menu()
+# menu()
     
     
 

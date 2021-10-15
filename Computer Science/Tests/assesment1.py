4f8ecebae18505958ed627999a2544743de24c8c
dan <80271815+poonchoi@users.noreply.github.com>
2021-10-15 12:14:45 +0200

    Update assesment1.py

--- Computer Science/Tests/assesment1.py
+++ Computer Science/Tests/assesment1.py
@@ -30,7 +30,7 @@ def number():
         isprime(User_number)
 
 
-'''
+
 
 # checks if the number is prime by cycling through the numbers smaller than the number and mod divides the number by the iteration. if it returns something then its a prime if its 0 it is 
 def isprime(User_number):
@@ -60,7 +60,7 @@ def repeat():
     
 number()
         
-'''
+
 
    
 

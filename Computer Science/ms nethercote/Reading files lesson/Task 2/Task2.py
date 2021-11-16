7ef251e252eeaac4f8b30d61841e9791cf8be931
dan <80271815+poonchoi@users.noreply.github.com>
2021-11-16 08:56:06 +0100

    Update Task2.py

--- Computer Science/ms nethercote/Reading files lesson/Task 2/Task2.py
+++ Computer Science/ms nethercote/Reading files lesson/Task 2/Task2.py
@@ -1,4 +1,2 @@
-'''
-in task 2 you must read the file Task2.txt and display how many
-times the letters A, S, D, F show up in the file (including lower case)
-'''
+# in task 2 you must read the file Task2.txt and display how many
+# times the letters A, S, D, F show up in the file (including lower case)
